4c93105809fa5005b2a71e61375e9ed8945d465c
User <user@Users-MacBook.local>
2023-08-29 11:54:04 +0200

    add

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -6,11 +6,11 @@ import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n: int, max_dealy: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     '''messure time wait_n'''
     start_time = time.time()
 
-    asyncio.run(wait_n(n, max_dealy))
+    asyncio.run(wait_n(n, max_delay))
     end_time = time.time()
     elapse_time = end_time - start_time
     return elapse_time / n
