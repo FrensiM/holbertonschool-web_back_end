b715b0e96b01ab3c5ca7cbe086a9c828d9c04b61
User <user@Users-MacBook.local>
2023-08-29 10:17:35 +0200

    add

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: float=10) -> float:
     '''wait randin func'''
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
