e54be3bde8f7c6222e5cca6a97dd8667a486b23c
User <user@Users-MacBook.local>
2023-08-31 12:02:20 +0200

    async generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,8 +5,8 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[int, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     ''' async generator '''
-    for _ in range(10):
+    for i in range(10):
         await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        yield random.uniform(0, 10)
