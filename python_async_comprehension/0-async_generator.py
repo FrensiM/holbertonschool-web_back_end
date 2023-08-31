4d7dc005e500c218ebd43c17ebae8a2ec3d173b4
User <user@Users-MacBook.local>
2023-08-31 11:56:18 +0200

    try

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''async file'''
+import asyncio
+import random
+
+async def async_generator():
+    ''' async generator '''
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.randint(0, 10)
