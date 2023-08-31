7540efcb144f251217b3860cb94afc6da868a226
User <user@Users-MacBook.local>
2023-08-31 11:58:55 +0200

    async generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,8 +2,10 @@
 '''async file'''
 import asyncio
 import random
+from typing import Generator
 
-async def async_generator():
+
+async def async_generator() -> Generator[int, None, None]:
     ''' async generator '''
     for _ in range(10):
         await asyncio.sleep(1)
