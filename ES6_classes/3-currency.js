b9c6c71f20654e151d1234aa73166ff926b3c57e
Frensi <user@Users-MacBook.local>
2023-09-15 11:51:27 +0200

    add

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,32 +1,31 @@
 export default class HolbertonCourse {
-    constructor(code, name) {
-        if (typeof code !== 'string') throw new Error('Code must be a string');
-        if (typeof name !== 'string') throw new Error('Name must be a string');
-        this._code = code;
-        this._name = name;
-    }
-
-    get code() {
-        return this._code;
-      }
-    
-      set code(code) {
-        if (typeof code !== 'string') {
-          throw new Error('Name must be a string');
-        }
-        this._length = length;
-      }
-    get name() {
-      return this._name;
+  constructor(code, name) {
+      if (typeof code !== 'string') throw new Error('Code must be a string');
+      if (typeof name !== 'string') throw new Error('Name must be a string');
+      this._code = code;
+      this._name = name;
+  }  
+  get code() {
+      return this._code;
     }
   
-    set name(name) {
-      if (typeof name !== 'string') {
+    set code(code) {
+      if (typeof code !== 'string') {
         throw new Error('Name must be a string');
       }
-      this._name = name;
+      this._length = length;
     }
-    displayFullCurrency() {
-        return `${this._name} (${this._code})`;
-      }
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    this._name = name;
+  }
+  displayFullCurrency() {
+      return `${this._name} (${this._code})`;
+    }
 }
