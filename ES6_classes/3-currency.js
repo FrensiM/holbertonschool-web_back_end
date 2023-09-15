3baa60d033cf03e6908d4e7cf7108853a3f2cbf8
Frensi <user@Users-MacBook.local>
2023-09-15 11:57:09 +0200

    add

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,19 +1,20 @@
 export default class HolbertonCourse {
   constructor(code, name) {
-      if (typeof code !== 'string') throw new Error('Code must be a string');
-      if (typeof name !== 'string') throw new Error('Name must be a string');
-      this._code = code;
-      this._name = name;
-  }  
+    if (typeof code !== 'string') throw new Error('Code must be a string');
+    if (typeof name !== 'string') throw new Error('Name must be a string');
+    this._code = code;
+    this._name = name;
+  }
+
   get code() {
-      return this._code;
+    return this._code;
+  }
+
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw new Error('Name must be a string');
     }
-  
-    set code(code) {
-      if (typeof code !== 'string') {
-        throw new Error('Name must be a string');
-      }
-      this._length = length;
+    this._length = length;
     }
   get name() {
     return this._name;
