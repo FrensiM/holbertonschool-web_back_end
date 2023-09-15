6bdc5abeb5568154ac0bc586c2f236efb70e0de1
Frensi <user@Users-MacBook.local>
2023-09-15 12:28:44 +0200

    add

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,13 +1,13 @@
-import Building from "./5-building";
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this.__floors = floors;
-  }  
+    this._floors = floors;
+  }
 
   get floors() {
-    return this.__floors;
+    return this._floors;
   }
 
   get sqft() {
@@ -16,5 +16,5 @@ export default class SkyHighBuilding extends Building {
 
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this._floors} floors`;
-    }
   }
+}
