0a44dc9cbb0215067cdf18e030ab39ec0a7b3b24
Frensi <user@Users-MacBook.local>
2023-10-03 12:16:56 +0200

    add

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,9 +2,19 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+    const promises = [];
 
-    return Promise.allSettled(promises).then((results) => {
-        console.log(results);
+    // Call signUpUser and push its promise to the promises array
+    promises.push(signUpUser(firstName, lastName));
+
+    // Call uploadPhoto and push its promise to the promises array
+    promises.push(uploadPhoto(fileName));
+
+    // Use Promise.allSettled to wait for all promises to settle
+    return Promise.allSettled(promises).then(results => {
+        return results.map(result => ({
+            status: result.status,
+            value: result.status === 'fulfilled' ? result.value : result.reason,
+        }));
     });
 }
