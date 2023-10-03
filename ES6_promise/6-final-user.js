89c5ec9b1dc96ae5671959ea0736890a545a7764
Frensi <user@Users-MacBook.local>
2023-10-03 12:06:50 +0200

    fixed'

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,12 +1,10 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-function handleProfileSignup(firstName, lastName, fileName) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
 
   return Promise.allSettled(promises).then((results) => {
     console.log(results)
   });
 }
-
-export default handleProfileSignup
